da9ba893422618ae1a0d62c783aa7f901d8deec5
DathatreyaKV <dathatreyakv@gmail.com>
2020-06-06 10:32:41 +0530

    Adding Eureka-Discovery server

---
+++ photoAppDiscoveryService/src/test/java/com/appsdeveloperblog/photoApp/ws/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoApp.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
