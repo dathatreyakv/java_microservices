dc12a49b1d1be05cf48d629fbd2fef24d250df57
DathatreyaKV <dathatreyakv@gmail.com>
2020-06-06 10:31:06 +0530

    adding account managing microservice

---
+++ photoAppApiAccountManagent/src/main/java/com/appsdeveloperblog/photoApp/api/account/PhotoAppApiAccountManagentApplication.java
@@ -0,0 +1,15 @@
+package com.appsdeveloperblog.photoApp.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoAppApiAccountManagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountManagentApplication.class, args);
+	}
+
+}
---
+++ photoAppApiAccountManagent/src/test/java/com/appsdeveloperblog/photoApp/api/account/PhotoAppApiAccountManagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoApp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
